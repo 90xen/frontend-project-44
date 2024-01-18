2d66400c15fa43223b84a7040c872c92f544e2fe
90xen <nijuuichi@yandex.ru>
2024-01-18 17:41:00 +0300

    finally, some good uuhh.. games

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,7 +1,10 @@
 #!/usr/bin/env node
 import hello from '../src/cli.js';
+import thisNumberEven from '../bin/brain-even.js';
+
 
 hello();
+thisNumberEven();
 // console.log('Welcome to the Brain Games!');
 // var userName = readlineSync.question('May I have your name? ');
 // console.log('Hello, ' + userName + '!');
--- src/cli.js
+++ src/cli.js
@@ -3,9 +3,8 @@ import readlineSync from 'readline-sync';
 function hello() {
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
-  return `Hello, ${userName}!`;
+  console.log(`Hello, ${userName}!`);
+  return userName;
 }
-//console.log(hello());
+
 export default hello;
-// const userName = readlineSync.question('May I have your name? ');
-// console.log('Hello, ' + userName + '!');
