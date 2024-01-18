eee8223ac833d2b8651ed049194385b12c0d961c
90xen <nijuuichi@yandex.ru>
2024-01-18 14:23:56 +0300

    help

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
 import hello from '../src/cli.js';
-//console.log('Welcome to the Brain Games!');
-//var userName = readlineSync.question('May I have your name? ');
-//console.log('Hello, ' + userName + '!');
+
+hello();
+// console.log('Welcome to the Brain Games!');
+// var userName = readlineSync.question('May I have your name? ');
+// console.log('Hello, ' + userName + '!');
